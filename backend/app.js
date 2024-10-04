1f25844aa2b912f2913224e43e384a5f040f82e7
Langelozzi <lucasangelozzi32@gmail.com>
2024-10-04 14:36:26 -0700

    Add get endpoint and middleware

--- backend/app.js
+++ backend/app.js
@@ -9,20 +9,38 @@ class App {
     static requestCount = 0;
 
     static start() {
-        const server = new HttpServer();
+        const app = new HttpServer();
 
-        server.post('/api/definitions', (req, res) => {
+        app.use(this.incrementRequestCount);
+
+        app.post('/api/definitions', (req, res) => {
             try {
                 const body = JSON.parse(req.body);
                 this.dictionary.addBatch(body);
+                res.status(201).send("Entry created");
             } catch (e) {
                 res.status(400).send(e);
             }
-        })
-
-        server.listen(server.DEFAULT_PORT, () => {
-            console.log(`Server listening on port ${server.DEFAULT_PORT}`);
         });
+
+        app.get('/api/definitions', (req, res) => {
+            const key = req.queryParams.word;
+
+            try {
+                const definition = this.dictionary.read(key);
+                res.status(200).send(definition);
+            } catch (e) {
+                res.status(400).send(e);
+            }
+        });
+
+        app.listen(app.DEFAULT_PORT, () => {
+            console.log(`Server listening on port ${app.DEFAULT_PORT}`);
+        });
+    }
+
+    static incrementRequestCount() {
+        this.requestCount++;
     }
 }
 
